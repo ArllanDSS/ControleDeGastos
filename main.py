4f1eda7685e2fa0a16755cd9359e15e33613642b
Arllan Santana <arllandavid@gmail.com>
2022-06-05 22:15:37 +0000

    Criado mostrarTabela()

--- main.py
+++ main.py
@@ -1,17 +1,51 @@
 from datetime import datetime
 
+despesas = [{
+    "ID": 1000,
+    "Data": '05/06/2022',
+    "Horario": '02:43',
+    "Despesa": 'UBER BLACK VIP EE',
+    "Valor": 1.00,
+    "Descricao": 'Lorem ipsum dolor sit amet. '
+}, {
+    "ID": 1000,
+    "Data": '05/06/2022',
+    "Horario": '02:43',
+    "Despesa": 'UBER',
+    "Valor": 10.00,
+    "Descricao": 'Lorem ipsum dolor sit amet. '
+}, {
+    "ID": 1000,
+    "Data": '05/06/2022',
+    "Horario": '02:43',
+    "Despesa": 'UBER BLACK VIP',
+    "Valor": 100.00,
+    "Descricao": 'Lorem ipsum dolor sit amet. '
+}, {
+    "ID": 1000,
+    "Data": '05/06/2022',
+    "Horario": '02:43',
+    "Despesa": 'UBER BLACK VIP',
+    "Valor": 1000.00,
+    "Descricao": 'Lorem ipsum dolor sit amet. '
+}, {
+    "ID": 1000,
+    "Data": '05/06/2022',
+    "Horario": '02:43',
+    "Despesa": 'UBER BLACK VIP',
+    "Valor": 10000.00,
+    "Descricao": 'Lorem ipsum dolor sit amet. '
+}]
 tabela = [
-    '----------------------------------------------------------------',
-    '                       RELATÓRIO DE DESPESAS                    ',
-    '----------------------------------------------------------------',
-    ' ID       Data       Horário         Despesa           Valor    ',
-    '----  ------------  ----------  -----------------  -------------'
+    '---------------------------------------------------------------',
+    '                      RELATÓRIO DE DESPESAS                    ',
+    '---------------------------------------------------------------',
+    ' ID       Data       Horário        Despesa            Valor   ',
+    '----  ------------  ---------  -----------------  -------------'
 ]
-despesas = []
 
 
 def addDespesa():
-
     id, dt, time, desp, val, desc = 0, str(), str(), str(), str(), str()
     # Entrada e Validação da data
 
@@ -62,3 +96,35 @@ def addDespesa():
         "Valor": f'{float(val):.2f}',
         "Descricao": desc
     })
+
+
+def mostrarTabela():
+
+    for n in tabela:
+        print(n)
+    for i in despesas:
+        tooString = str()
+        cont = 0
+        v_aux = 0
+        for j in i:
+            if cont == 0:
+                tooString += str(i[j]).ljust(7)
+            elif cont == 1:
+                tooString += str(i[j]).rjust(10)
+            elif cont == 2:
+                tooString += str(i[j]).rjust(10)
+            elif cont == 3:
+                tooString += str(i[j]).rjust(13 + len(i[j]) // 2)
+                v_aux = (17 - (len(i[j]))) // 2
+            elif cont == 4:
+                v = f'R$ {i[j]:.2f}'
+                v_len = len(v) - 7 // 2 + 7
+                v_aux += v_len
+                tooString += f'{v:>{v_aux}}'
+                break
+            cont += 1
+
+        print(tooString)
+
+
+mostrarTabela()
